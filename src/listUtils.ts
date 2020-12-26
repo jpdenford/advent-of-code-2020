07c687296a1d0ddfcbcba637351b1e09da4fe3e1
jpdenford <jpdenford@gmail.com>
2020-12-26 23:48:03 +1300

    Fix list utils tests

--- src/listUtils.ts
+++ src/listUtils.ts
@@ -4,8 +4,6 @@ export const splitWhenEvery = (predicate: (first?: number, second?:number) => bo
   if(ns.length === 0) return []
   let chunk: number[] = []
   for(let i = 0; i < ns.length; i++) {
-    // console.log('nsi', i, ns[i])
-    // if(predicate(ns[i], ns[i + 1]) || predicate(ns[i - 1], ns[i])) {
     chunk = chunk.concat(ns[i])
     if(predicate(ns[i], ns[i + 1])) {
       const remainingChunks = splitWhenEvery(predicate)(ns.slice(i + 1))
--- src/listUtils.unit.ts
+++ src/listUtils.unit.ts
@@ -5,28 +5,28 @@ test('splitWhenEvery/ single split', (t) => {
   t.plan(1)
   const input = [0, 1, 2, 3, 3, 4, 5]
   const output = splitWhenEvery((n1, n2) => (n2 ?? 0) - (n1 ?? 0) === 0)(input)
-  t.isEquivalent(output, [[0, 1, 2], [3, 4, 5]])
+  t.isEquivalent(output, [[ 0, 1, 2, 3 ], [ 3, 4, 5 ]])
 })
 
 test('splitWhenEvery/ multi split', (t) => {
   t.plan(1)
   const input = [0, 1, 2, 3, 3, 4, 5, 5, 6]
   const output = splitWhenEvery((n1, n2) => (n2 ?? 0) - (n1 ?? 0) === 0)(input)
-  t.isEquivalent(output, [[0, 1, 2], [3, 4], [5, 6]])
+  t.isEquivalent(output, [[ 0, 1, 2, 3 ], [ 3, 4, 5 ], [ 5, 6 ]])
 })
 
 test('splitWhenEvery/ multi split 2', (t) => {
   t.plan(1)
   const input = [0, 1, 2, 3, 3, 4, 5, 5, 5, 5, 6]
   const output = splitWhenEvery((n1, n2) => (n2 ?? 0) - (n1 ?? 0) === 0)(input)
-  t.isEquivalent(output, [[0, 1, 2], [3, 4], [6]])
+  t.isEquivalent(output, [[ 0, 1, 2, 3 ], [ 3, 4, 5 ], [ 5 ], [ 5 ], [ 5, 6 ]])
 })
 
 test('splitWhenEvery/ multi sequential split', (t) => {
   t.plan(1)
   const input = [0, 1, 2, 3, 3, 4, 5, 5, 5, 6]
   const output = splitWhenEvery((n1, n2) => (n2 ?? 0) - (n1 ?? 0) === 0)(input)
-  t.isEquivalent(output, [[0, 1, 2], [3, 4], [5, 6]])
+  t.isEquivalent(output,  [ [ 0, 1, 2, 3 ], [ 3, 4, 5 ], [ 5 ], [ 5, 6 ] ])
 })
 
 test('splitWhenEvery/ empty list', (t) => {
@@ -47,11 +47,11 @@ test('splitWhenEvery/ split only', (t) => {
   t.plan(1)
   const input: number[] = [1, 1]
   const output = splitWhenEvery((n1, n2) => n1 == n2)(input)
-  t.isEquivalent(output, [[1]])
+  t.isEquivalent(output, [[1],[1]])
 })
 
 
-test.only('splitWhenEvery/ xxxx', (t) => {
+test('splitWhenEvery/ xxxx', (t) => {
   t.plan(1)
   const input: number[] = [
     1,  2,  3,  4,  7,  8,  9, 10, 11,
@@ -60,5 +60,9 @@ test.only('splitWhenEvery/ xxxx', (t) => {
    46, 47, 48, 49
  ]
   const output = splitWhenEvery((n1, n2) => (n2 ?? -99) - (n1 ?? -99) >= 3)(input)
-  t.isEquivalent(output, [[2, 3], ])
+  t.isEquivalent(output,  [
+    [ 1, 2, 3, 4 ], [ 7, 8, 9, 10, 11 ],
+    [ 14 ], [ 17, 18, 19, 20 ], [ 23, 24, 25 ], [ 28 ],
+    [ 31, 32, 33, 34, 35 ], [ 38, 39 ], [ 42 ],
+    [ 45, 46, 47, 48, 49 ] ])
 })
