0e60aa434f487a461d356deb59405f9642539dbd
jpdenford <jpdenford@gmail.com>
2020-12-26 23:40:30 +1300

    Cleanup 10

--- src/day10/adapterArray.ts
+++ src/day10/adapterArray.ts
@@ -23,27 +23,15 @@ export const multiplyCounts = (diffs: number[]): number => {
   return R.reduce(R.multiply as any, 1, counts)
 }
 
-const calculateDiffsWithExtra = R.pipe(addBuiltInAdapterAndOutlet, calculateGaps)
+const calculateDiffsWithPadding = R.pipe(addBuiltInAdapterAndOutlet, calculateGaps)
 
 export const calculateProductOfCounts = R.pipe(
-  calculateDiffsWithExtra,
+  calculateDiffsWithPadding,
   R.tap(console.log),
   multiplyCounts
 )
 
-const countSequences = (ns: number[]): { num: number; count: number }[] => {
-  return ns
-    .reduce((counts: { num: number; count: number }[], next: number) => {
-      const previous = R.head(counts)!
-      if (previous && previous.num === next)
-        return [
-          { num: previous.num, count: previous.count + 1 },
-          ...R.tail(counts),
-        ]
-      else return [{ num: next, count: 1 }, ...counts]
-    }, [])
-    .reverse()
-}
+export const main1 = R.pipe(readIn, calculateProductOfCounts, JSON.stringify)
 
 const asString = (ns: number[]) => ns.join(',')
 
@@ -72,17 +60,6 @@ export const findRemovalsForRange = (numbers: number[]): string[] => {
   return recurseSubstitutes(sorted).filter(x => !!x)
 }
 
-export const main1 = R.pipe(readIn, calculateProductOfCounts, JSON.stringify)
-
-export const calculateCombinations = R.pipe(
-  calculateDiffsWithExtra,
-  countSequences,
-  R.filter((c: { num: number }) => c.num === 1),
-  R.map((c) => c.count),
-  R.reduce(R.multiply, 1)
-)
-
-
 export const validCombinations = (ns: number[]) => {
   const withFirstAndLast = addBuiltInAdapterAndOutlet(ns)
   const sorted = withFirstAndLast.sort(R.ascend(R.identity))
@@ -96,19 +73,19 @@ export const validCombinations = (ns: number[]) => {
 
 export const main2 = R.pipe(readIn, validCombinations, JSON.stringify)
 
+// some examples of sequential chunk combinations
 
-// some examples
+// 0 1 2
+// 0 _ 2
+//d 1 1
+// combs 2
 
-// 0 3 4 5 8
-// 0 3 _ 5 8
-//d 3 1 1 3 -> 2
-
-// 0 3 4 5 6 9
-// 0 3 _ 5 6 9
-// 0 3 - - 6 9
-// 0 3 4 _ 6 9
-//d 3 1 1 1 3 -> 4
-
+// 0 1 2 3
+// 0 _ 2 3
+// 0 - - 3
+// 0 1 _ 3
+//d 1 1 1
+// combs 4
 
 // 0 1 2 3 4
 // 0 _ 2 3 4
@@ -117,75 +94,5 @@ export const main2 = R.pipe(readIn, validCombinations, JSON.stringify)
 // 0 1 _ _ 4
 // 0 1 2 _ 4
 // 0 _ 2 _ 4
-//d 1 1 1 1 -> 7
-
-// 0 3 4 5 6 7 8 11
-// 0 3 _ 5 6 7 8 11
-// 0 3 _ _ 6 7 8 11
-// 0 3 4 _ 6 7 8 11
-// 0 3 4 _ _ 7 8 11
-// 0 3 4 5 _ 7 8 11 ======
-// 0 3 4 5 _ _ 8 11
-// 0 3 4 5 6 _ 8 11
-// 0 3 _ 5 6 _ 8 11
-// 0 3 _ _ 6 _ 8 11
-// 0 3 4 _ 6 _ 8 11
-//d 3 1 1 1 1 1 3 -> 11
-
-
-// up to nine
-// 0 3 4 5 6 7 8 9 12
-// 0 3 _ 5 6 7 8 9 12
-// 0 3 _ _ 6 7 8 9 12
-// 0 3 4 _ 6 7 8 9 12
-// 0 3 4 _ _ 7 8 9 12
-// 0 3 4 5 _ 7 8 9 12
-// 0 3 4 5 _ _ 8 9 12
-// 0 3 4 5 6 _ 8 9 12
-// 0 3 _ 5 6 _ 8 9 12
-// 0 3 _ _ 6 _ 8 9 12
-// 0 3 4 _ 6 _ 8 9 12 ..
-// 0 3 4 5 6 _ _ 9 12
-// 0 3 _ 5 6 _ _ 9 12
-// 0 3 _ _ 6 _ _ 9 12
-// 0 3 4 _ 6 _ _ 9 12
-// 0 3 4 5 6 7 _ 9 12
-// 0 3 _ 5 6 7 _ 9 12
-// 0 3 _ _ 6 7 _ 9 12
-// 0 3 4 _ 6 7 _ 9 12
-// 0 3 4 _ _ 7 _ 9 12
-//d 3 1 1 1 1 1 1 3 -> 20
-
-
-// up to 10
-// 0 3 4 5 6 7 8 9 10 13
-// 0 3 _ 5 6 7 8 9 10 13
-// 0 3 _ _ 6 7 8 9 10 13
-// 0 3 4 _ 6 7 8 9 10 13
-// 0 3 4 _ _ 7 8 9 10 13
-// 0 3 4 5 _ 7 8 9 10 13
-// 0 3 4 5 _ _ 8 9 10 13
-// 0 3 4 5 6 _ 8 9 10 13
-// 0 3 _ 5 6 _ 8 9 10 13
-// 0 3 _ _ 6 _ 8 9 10 13
-// 0 3 4 _ 6 _ 8 9 10 13
-// 0 3 4 5 6 _ _ 9 10 13
-// 0 3 _ 5 6 _ _ 9 10 13
-// 0 3 _ _ 6 _ _ 9 10 13
-// 0 3 4 _ 6 _ _ 9 10 13
-// 0 3 4 5 6 7 _ 9 10 13
-// 0 3 _ 5 6 7 _ 9 10 13
-// 0 3 _ _ 6 7 _ 9 10 13
-// 0 3 4 _ 6 7 _ 9 10 13
-// 0 3 4 _ _ 7 _ 9 10 13 ..
-// 0 3 4 5 6 7 _ _ 10 13
-// 0 3 _ 5 6 7 _ _ 10 13
-// 0 3 _ 5 _ 7 _ _ 10 13
-// 0 3 _ _ 6 7 _ _ 10 13
-// 0 3 4 _ 6 7 _ _ 10 13
-// 0 3 4 5 6 7 8 _ 10 13
-// 0 3 _ 5 6 7 8 _ 10 13
-// 0 3 _ 5 _ 7 8 _ 10 13
-// 0 3 _ _ 6 7 8 _ 10 13
-// 0 3 4 _ 6 7 8 _ 10 13
-//d 3 1 1 1 1 1 1 3 -> 30
+//d 1 1 1 1
+// combs 7
